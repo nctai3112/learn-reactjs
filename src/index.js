ce54a85dba9c7a9ed93c9bca7d2c6f51213a1190
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2023-03-23 09:26:11 +0700

    Google Sign In implementation

--- src/index.js
+++ src/index.js
@@ -1,133 +1,156 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { Stage, Layer, Rect, Transformer } from "react-konva";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import reportWebVitals from "./reportWebVitals";
 
-const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
-  const shapeRef = React.useRef();
-  const trRef = React.useRef();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <GoogleOAuthProvider clientId="482831900623-fvngfj4petn48ms9a9jqvmod19iglr5r.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
+);
 
-  React.useEffect(() => {
-    if (isSelected) {
-      // we need to attach transformer manually
-      trRef.current.nodes([shapeRef.current]);
-      trRef.current.getLayer().batchDraw();
-    }
-  }, [isSelected]);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
-  return (
-    <React.Fragment>
-      <Rect
-        onClick={onSelect}
-        onTap={onSelect}
-        ref={shapeRef}
-        {...shapeProps}
-        draggable
-        onDragEnd={(e) => {
-          onChange({
-            ...shapeProps,
-            x: e.target.x(),
-            y: e.target.y(),
-          });
-        }}
-        onTransformEnd={(e) => {
-          // transformer is changing scale of the node
-          // and NOT its width or height
-          // but in the store we have only width and height
-          // to match the data better we will reset scale on transform end
-          const node = shapeRef.current;
-          const scaleX = node.scaleX();
-          const scaleY = node.scaleY();
+// COMMENT CODE DEMO KONVA TEMPORARY.
 
-          // we will reset it back
-          node.scaleX(1);
-          node.scaleY(1);
-          onChange({
-            ...shapeProps,
-            x: node.x(),
-            y: node.y(),
-            // set minimal value
-            width: Math.max(5, node.width() * scaleX),
-            height: Math.max(node.height() * scaleY),
-          });
-        }}
-      />
-      {isSelected && (
-        <Transformer
-          ref={trRef}
-          boundBoxFunc={(oldBox, newBox) => {
-            // limit resize
-            if (newBox.width < 5 || newBox.height < 5) {
-              return oldBox;
-            }
-            return newBox;
-          }}
-        />
-      )}
-    </React.Fragment>
-  );
-};
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import { Stage, Layer, Rect, Transformer } from "react-konva";
 
-const initialRectangles = [
-  {
-    x: 10,
-    y: 10,
-    width: 100,
-    height: 100,
-    fill: "red",
-    id: "rect1",
-  },
-  {
-    x: 150,
-    y: 150,
-    width: 100,
-    height: 100,
-    fill: "green",
-    id: "rect2",
-  },
-];
+// const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
+//   const shapeRef = React.useRef();
+//   const trRef = React.useRef();
 
-const App = () => {
-  const [rectangles, setRectangles] = React.useState(initialRectangles);
-  const [selectedId, selectShape] = React.useState(null);
+//   React.useEffect(() => {
+//     if (isSelected) {
+//       // we need to attach transformer manually
+//       trRef.current.nodes([shapeRef.current]);
+//       trRef.current.getLayer().batchDraw();
+//     }
+//   }, [isSelected]);
 
-  const checkDeselect = (e) => {
-    // deselect when clicked on empty area
-    const clickedOnEmpty = e.target === e.target.getStage();
-    if (clickedOnEmpty) {
-      selectShape(null);
-    }
-  };
+//   return (
+//     <React.Fragment>
+//       <Rect
+//         onClick={onSelect}
+//         onTap={onSelect}
+//         ref={shapeRef}
+//         {...shapeProps}
+//         draggable
+//         onDragEnd={(e) => {
+//           onChange({
+//             ...shapeProps,
+//             x: e.target.x(),
+//             y: e.target.y(),
+//           });
+//         }}
+//         onTransformEnd={(e) => {
+//           // transformer is changing scale of the node
+//           // and NOT its width or height
+//           // but in the store we have only width and height
+//           // to match the data better we will reset scale on transform end
+//           const node = shapeRef.current;
+//           const scaleX = node.scaleX();
+//           const scaleY = node.scaleY();
 
-  return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onMouseDown={checkDeselect}
-      onTouchStart={checkDeselect}
-    >
-      <Layer>
-        {rectangles.map((rect, i) => {
-          return (
-            <Rectangle
-              key={i}
-              shapeProps={rect}
-              isSelected={rect.id === selectedId}
-              onSelect={() => {
-                selectShape(rect.id);
-              }}
-              onChange={(newAttrs) => {
-                const rects = rectangles.slice();
-                rects[i] = newAttrs;
-                setRectangles(rects);
-              }}
-            />
-          );
-        })}
-      </Layer>
-    </Stage>
-  );
-};
+//           // we will reset it back
+//           node.scaleX(1);
+//           node.scaleY(1);
+//           onChange({
+//             ...shapeProps,
+//             x: node.x(),
+//             y: node.y(),
+//             // set minimal value
+//             width: Math.max(5, node.width() * scaleX),
+//             height: Math.max(node.height() * scaleY),
+//           });
+//         }}
+//       />
+//       {isSelected && (
+//         <Transformer
+//           ref={trRef}
+//           boundBoxFunc={(oldBox, newBox) => {
+//             // limit resize
+//             if (newBox.width < 5 || newBox.height < 5) {
+//               return oldBox;
+//             }
+//             return newBox;
+//           }}
+//         />
+//       )}
+//     </React.Fragment>
+//   );
+// };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+// const initialRectangles = [
+//   {
+//     x: 10,
+//     y: 10,
+//     width: 100,
+//     height: 100,
+//     fill: "red",
+//     id: "rect1",
+//   },
+//   {
+//     x: 150,
+//     y: 150,
+//     width: 100,
+//     height: 100,
+//     fill: "green",
+//     id: "rect2",
+//   },
+// ];
+
+// const App = () => {
+//   const [rectangles, setRectangles] = React.useState(initialRectangles);
+//   const [selectedId, selectShape] = React.useState(null);
+
+//   const checkDeselect = (e) => {
+//     // deselect when clicked on empty area
+//     const clickedOnEmpty = e.target === e.target.getStage();
+//     if (clickedOnEmpty) {
+//       selectShape(null);
+//     }
+//   };
+
+//   return (
+//     <Stage
+//       width={window.innerWidth}
+//       height={window.innerHeight}
+//       onMouseDown={checkDeselect}
+//       onTouchStart={checkDeselect}
+//     >
+//       <Layer>
+//         {rectangles.map((rect, i) => {
+//           return (
+//             <Rectangle
+//               key={i}
+//               shapeProps={rect}
+//               isSelected={rect.id === selectedId}
+//               onSelect={() => {
+//                 selectShape(rect.id);
+//               }}
+//               onChange={(newAttrs) => {
+//                 const rects = rectangles.slice();
+//                 rects[i] = newAttrs;
+//                 setRectangles(rects);
+//               }}
+//             />
+//           );
+//         })}
+//       </Layer>
+//     </Stage>
+//   );
+// };
+
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(<App />);
