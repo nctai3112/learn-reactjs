b7ae112fde98f5f5568762a6b6387239bc5a0e97
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2023-04-04 08:12:01 +0700

    Dev: toolbar image

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Canvas from "./features/Annotation";
 import Polygon from "./features/Polygon";
 // import Button from "./features/Annotation/components/ButtonComponent";
 import ImageWithRectangle from "./components/DemoComponent";
+import KonvaToolbar from "./components/ImageWithToolbar";
 
 const _img =
   "https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/2x1_NSwitch_TloZTearsOfTheKingdom_Gamepage_image1600w.jpg";
@@ -14,11 +15,13 @@ function App() {
       <NavLink to="/login">Login</NavLink>
       <NavLink to="/annotation"> - Annotation</NavLink>
       <NavLink to="/demo-bounding-boxes"> - Demo Bounding Box</NavLink>
+      <NavLink to="/demo-toolbar-image"> - Demo Toolbar Image</NavLink>
 
       <Routes>
         <Route path="/login" element={<LoginFeature />} />
         <Route path="/annotation" element={<Canvas />} />
         <Route path="/demo-bounding-boxes" element={<ImageWithRectangle />} />
+        <Route path="/demo-toolbar-image" element={<KonvaToolbar />} />
         <Route
           path="/test-polygon"
           element={<Polygon imageUrl={_img}></Polygon>}
