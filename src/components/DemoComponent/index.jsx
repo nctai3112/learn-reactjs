43183dcbfbc1df6fe25f62ed63dfddbfb4f4a0dc
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2023-04-08 00:43:54 +0700

    Stable version handling boundingBoxes after dragging

--- src/components/DemoComponent/index.jsx
+++ src/components/DemoComponent/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Layer, Rect } from "react-konva";
+import { Layer, Rect, Group } from "react-konva";
 import BaseImageComponent from "../../features/BaseImageComponent";
 
 const ImageWithRectangle = () => {
@@ -16,6 +16,9 @@ const ImageWithRectangle = () => {
   const [isEditing, setIsEditing] = useState(false);
   // Array to store all bounding boxes.
   const [boundingBoxes, setBoundingBoxes] = useState([]);
+
+  const _id = String(Math.random());
+  const _name = `group-${_id}`;
 
   const imageUrl =
     "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg";
@@ -47,6 +50,13 @@ const ImageWithRectangle = () => {
     // Mouse down on rect == end drawing a rect.
     if (isEditing) {
       setBoundingBoxes([...boundingBoxes, rect]);
+      setRect({
+        id: "0",
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+      });
       setIsEditing(false);
     }
   };
@@ -67,23 +77,64 @@ const ImageWithRectangle = () => {
     }
   };
 
+  const handleGroupMouseOver = (e) => {
+    if (isEditing) return;
+    e.target.getStage().container().style.cursor = "move";
+  };
+
+  const handleGroupMouseOut = (e) => {
+    e.target.getStage().container().style.cursor = "default";
+  };
+
+  const handleRectDragEnd = (e) => {
+    const newBoundingBox = boundingBoxes.map((boundingBox) => {
+      if (boundingBox.id == e.target.id()) {
+        boundingBox.x = e.target.x();
+        boundingBox.y = e.target.y();
+        boundingBox.width = e.target.width();
+        boundingBox.height = e.target.height();
+      }
+      return boundingBox;
+    });
+    setBoundingBoxes(newBoundingBox);
+  };
+
+  // Not use yet.
+  const handleGroupDragStart = (e) => {};
+  const handleGroupDragMove = (e) => {};
+  const groupDragBound = (e) => {};
+
   const renderBoundingBoxes = () => {
     if (!boundingBoxes.length) return <></>;
     return boundingBoxes
       .filter((prop) => prop.x !== 0 && prop.y !== 0)
       .map(({ id, x, y, width, height }) => {
         return (
-          <Rect
-            onMouseDown={handleMouseDownOnRect}
-            key={String(id)}
-            id={String(id)}
-            x={x}
-            y={y}
-            width={width}
-            height={height}
-            stroke="black"
-            strokeWidth={3}
-          />
+          <Group
+            id={_id}
+            name={"bounding_box"}
+            draggable={!isEditing}
+            onMouseOver={handleGroupMouseOver}
+            onMouseOut={handleGroupMouseOut}
+            // dragBoundFunc={groupDragBound}
+          >
+            <Rect
+              onMouseDown={handleMouseDownOnRect}
+              onDragMove={handleGroupDragMove}
+              onDragStart={handleGroupDragStart}
+              onDragEnd={handleRectDragEnd}
+              key={String(id)}
+              id={String(id)}
+              x={x}
+              y={y}
+              draggable={true}
+              width={width}
+              height={height}
+              stroke="#00F1FF"
+              strokeWidth={3}
+              fill="rgb(140,30,255,0.5)"
+            />
+          </Group>
         );
       });
   };
@@ -102,7 +153,8 @@ const ImageWithRectangle = () => {
           y={rect.y}
           width={rect.width}
           height={rect.height}
-          stroke="red"
+          stroke="black"
+          strokeWidth={3}
           onMouseDown={handleMouseDownOnRect}
         />
         {renderBoundingBoxes()}
