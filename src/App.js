eadfcf1cf13195a53d812e79aa759ba5a7c09ecf
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2023-04-14 17:16:06 +0700

    Update: label function + antd apply

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import "./App.css";
 import { Route, NavLink, Routes } from "react-router-dom";
 import LoginFeature from "./features/Login";
 import Canvas from "./features/Annotation";
-// import Button from "./features/Annotation/components/ButtonComponent";
-import ImageWithRectangle from "./components/DemoComponent";
-import PolygonsAnnotation from "./features/Polygons";
 import AnnotationMerge from "./features/AnnotationMerge";
+import DemoPage from "./features/Demo";
 
 const _img =
   "https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/2x1_NSwitch_TloZTearsOfTheKingdom_Gamepage_image1600w.jpg";
@@ -14,19 +12,14 @@ function App() {
     <div className="App">
       <NavLink to="/login">Login</NavLink>
       <NavLink to="/annotation"> - Annotation</NavLink>
-      <NavLink to="/demo-bounding-boxes"> - Demo Bounding Box</NavLink>
+      <NavLink to="/annotation-ant"> - Annotation Ant</NavLink>
       <NavLink to="/demo-annotation-merge"> - Demo Annotation Merge</NavLink>
-      <NavLink to="/test-polygon"> - Demo Polygons</NavLink>
 
       <Routes>
         <Route path="/login" element={<LoginFeature />} />
         <Route path="/annotation" element={<Canvas />} />
-        <Route path="/demo-bounding-boxes" element={<ImageWithRectangle />} />
+        <Route path="/annotation-ant" element={<DemoPage />} />
         <Route path="/demo-annotation-merge" element={<AnnotationMerge />} />
-        <Route
-          path="/test-polygon"
-          element={<PolygonsAnnotation imageUrl={_img}></PolygonsAnnotation>}
-        />
       </Routes>
     </div>
   );
