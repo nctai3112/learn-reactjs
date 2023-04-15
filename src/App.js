4cb146cba40cf6056d9a2c0eede89a56744d3483
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2023-04-15 22:30:37 +0700

    branch for demo pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LoginFeature from "./features/Login";
 import Canvas from "./features/Annotation";
 import AnnotationMerge from "./features/AnnotationMerge";
 import DemoPage from "./features/Demo";
+import DragAndDrop from "./features/Demo/component/AnnotationAnt";
 
 const _img =
   "https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/2x1_NSwitch_TloZTearsOfTheKingdom_Gamepage_image1600w.jpg";
@@ -12,13 +13,15 @@ function App() {
     <div className="App">
       <NavLink to="/login">Login</NavLink>
       <NavLink to="/annotation"> - Annotation</NavLink>
-      <NavLink to="/annotation-ant"> - Annotation Ant</NavLink>
+      <NavLink to="/drag-and-drop"> - DragDrop</NavLink>
+      {/* <NavLink to="/annotation-ant"> - Annotation Ant</NavLink> */}
       <NavLink to="/demo-annotation-merge"> - Demo Annotation Merge</NavLink>
 
       <Routes>
         <Route path="/login" element={<LoginFeature />} />
         <Route path="/annotation" element={<Canvas />} />
-        <Route path="/annotation-ant" element={<DemoPage />} />
+        <Route path="/drag-and-drop" element={<DragAndDrop />} />
+        {/* <Route path="/annotation-ant" element={<DemoPage />} /> */}
         <Route path="/demo-annotation-merge" element={<AnnotationMerge />} />
       </Routes>
     </div>
--- src/features/Demo/component/AnnotationAnt/index.jsx
+++ src/features/Demo/component/AnnotationAnt/index.jsx
@@ -1,65 +1,48 @@
 import React, { useState } from "react";
-import { Stage, Layer, Rect } from "react-konva";
-import { Button, Modal, Form, Input } from "antd";
-import "antd/dist/antd";
+import { Stage, Layer, Circle, Image } from "react-konva";
 
-const AnnotationApp = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [rectangles, setRectangles] = useState([]);
+function DragAndDrop() {
+  const [isDragging, setIsDragging] = useState(false);
+  const [position, setPosition] = useState({ x: 50, y: 50 });
 
-  const handleAddRectangle = (values) => {
-    setRectangles([...rectangles, values]);
-    setIsModalVisible(false);
+  const handleDragStart = () => {
+    setIsDragging(true);
   };
 
+  const handleDragEnd = (e) => {
+    setIsDragging(false);
+    // Check if the image was dropped inside the circle
+    if (e.target.attrs.x > 200 && e.target.attrs.y > 200) {
+      setPosition({ x: 250, y: 250 });
+    } else {
+      setPosition({ x: 50, y: 50 });
+    }
+  };
+
+  const image = new window.Image();
+  image.src =
+    "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png";
+
   return (
-    <>
-      <Button onClick={() => setIsModalVisible(true)}>Add Rectangle</Button>
+    <div>
       <Stage width={500} height={500}>
         <Layer>
-          {rectangles.map(({ x, y, width, height, fill }) => (
-            <Rect
-              key={`${x}${y}`}
-              x={x}
-              y={y}
-              width={width}
-              height={height}
-              fill={fill}
-            />
-          ))}
+          <Circle x={250} y={250} radius={50} stroke="black" />
+          <Image
+            draggable
+            x={position.x}
+            y={position.y}
+            image={image}
+            width={50}
+            height={50}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+            opacity={isDragging ? 0.5 : 1}
+          />
         </Layer>
       </Stage>
-      <Modal
-        title="Add Rectangle"
-        visible={isModalVisible}
-        onCancel={() => setIsModalVisible(false)}
-        footer={null}
-      >
-        <Form onFinish={handleAddRectangle}>
-          <Form.Item label="X" name="x">
-            <Input type="number" />
-          </Form.Item>
-          <Form.Item label="Y" name="y">
-            <Input type="number" />
-          </Form.Item>
-          <Form.Item label="Width" name="width">
-            <Input type="number" />
-          </Form.Item>
-          <Form.Item label="Height" name="height">
-            <Input type="number" />
-          </Form.Item>
-          <Form.Item label="Fill" name="fill">
-            <Input type="color" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Add
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
-    </>
+    </div>
   );
-};
+}
 
-export default AnnotationApp;
+export default DragAndDrop;
