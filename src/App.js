505003cc859722b58bf054b7aaf7993ad1ba2999
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2023-05-08 19:22:45 +0700

    First successful version sending request to server to create author instance

--- src/App.js
+++ src/App.js
@@ -46,38 +46,6 @@ function App() {
       </Routes>
     </div>
   );
-  // const [user, setUser] = useState([]);
-  // const [profile, setProfile] = useState([]);
-
-  // const login = useGoogleLogin({
-  //   onSuccess: (codeResponse) => setUser(codeResponse),
-  //   onError: (error) => console.log("Login Failed:", error),
-  // });
-
-  // useEffect(() => {
-  //   if (user) {
-  //     axios
-  //       .get(
-  //         `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
-  //         {
-  //           headers: {
-  //             Authorization: `Bearer ${user.access_token}`,
-  //             Accept: "application/json",
-  //           },
-  //         }
-  //       )
-  //       .then((res) => {
-  //         setProfile(res.data);
-  //       })
-  //       .catch((err) => console.log(err));
-  //   }
-  // }, [user]);
-
-  // // log out function to log the user out of google and set the profile array to null
-  // const logOut = () => {
-  //   googleLogout();
-  //   setProfile(null);
-  // };
 
   // return (
   //   <div className="app-container">
