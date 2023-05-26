d777e6bceadcc47f51b4fcbce9c683fd7be0ed6d
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2023-05-26 18:10:06 +0700

    Redux persist to store login data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import loginSlice from "../features/Login/loginSlice";
 import projectDetailSlide from "../features/ProjectDetail/projectDetailSlice";
-
-const store = configureStore({
-  reducer: {
-    login: loginSlice.reducer,
-    projectDetail: projectDetailSlide.reducer,
-  },
+import { combineReducers } from "redux";
+// const store = configureStore({
+//   reducer: {
+//     login: loginSlice.reducer,
+//     projectDetail: projectDetailSlide.reducer,
+//   },
+// });
+const rootReducer = combineReducers({
+  login: loginSlice.reducer,
+  projectDetail: projectDetailSlide.reducer,
 });
 
-// import { createStore } from "redux";
-// import rootReducer from "./reducer";
+const persistConfig = {
+  key: "root", // key for the root level of the persisted state
+  storage, // storage engine to use
+  // Add any additional configuration options if needed
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// const store = createStore(rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  // any other configuration options you need
+});
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
