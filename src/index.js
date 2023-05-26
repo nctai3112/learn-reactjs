0766d497faf3c25edde77e86d9810950c5c3c031
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2023-05-26 18:10:06 +0700

    Redux persist to store login data

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,7 +15,9 @@ root.render(
     <React.StrictMode>
       <BrowserRouter>
         <Provider store={store}>
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </Provider>
       </BrowserRouter>
     </React.StrictMode>
